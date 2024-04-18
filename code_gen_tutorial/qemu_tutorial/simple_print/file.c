5e3b2aa0d1bc05d42ef0e7d89ae67fcae04df262
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-04-18 20:55:43 +0530

    added testcase simple print only with numbers and not expressions

---
+++ code_gen_tutorial/qemu_tutorial/simple_print/file.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+int x;
+
+int main(){
+	// x = 5;
+	// printf("%d %d %d %d %d", 5, 6, 7, 8, 9);
+	printf("%d ", 5);
+	printf("\n");
+	printf("%d ", 6);
+	printf("%d ", 7);
+	printf("%d ", 8);
+	// printf("\n");
+}
+
+
+
---
+++ code_gen_tutorial/qemu_tutorial/simple_print/file.s
@@ -0,0 +1,101 @@
+	.file	1 "file.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.globl	x
+	.section	.bss,"aw",@nobits
+	.align	2
+	.type	x, @object
+	.size	x, 4
+x:
+	.space	4
+	.rdata
+	.align	2
+$LC0:
+	.ascii	"%d \000"
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,32,$31		# vars= 0, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	sw	$fp,24($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	li	$5,5			# 0x5
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	li	$4,10			# 0xa
+	lw	$2,%call16(putchar)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,putchar
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	li	$5,6			# 0x6
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	li	$5,7			# 0x7
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	li	$5,8			# 0x8
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	move	$2,$0
+	move	$sp,$fp
+	lw	$31,28($sp)
+	lw	$fp,24($sp)
+	addiu	$sp,$sp,32
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 10.3.0-1ubuntu1) 10.3.0"
+	.section	.note.GNU-stack,"",@progbits
